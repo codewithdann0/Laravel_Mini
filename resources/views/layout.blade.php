e11a18c02597e2cbfc96b0dfa3ee73bab839e150
Daniel Tigistu <166249731+codewithdann0@users.noreply.github.com>
2024-10-27 05:29:31 -0700

    Query Parameters and How we access them

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    @endif
+</head>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <header class="text-center">
+        This is the Header
+    </header>
+
+    @yield("content")
+
+    <footer class="text-center p-20 bg-slate-800">
+        This is the Footer
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/pizza', function () {
     $pizzas = [
         ["type" => "Ethiopian Pizza", "price" => "57", "base" => "Meta and Egg"],
         ["type" => "Ethiopian Pizza", "price" => "57", "base" => "Meta and Egg"],
         ["type" => "Ethiopian Pizza", "price" => "57", "base" => "Meta and Egg"]
     ];
 
-    return view('Pizza', ['pizzas' => $pizzas]);
+    return view('Pizza', ['pizzas' => $pizzas, 'name' =>request('name'), 'age' => request('age')],);
 });
 
 
