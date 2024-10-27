e7e15bd98ccd7985cb5f0c32b35ce0df08ae48d7
Daniel Tigistu <166249731+codewithdann0@users.noreply.github.com>
2024-10-27 12:44:17 -0700

    Passing data to the controllers

---
+++ resources/views/details.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('content')
+<div>
+    <div class="align-center">
+        Pizza List- {{$id}}
+    </div>
+
+          
+</div>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,19 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PizzaController;
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pizza', function () {
-    $pizzas = [
-        ["type" => "Ethiopian Pizza", "price" => "57", "base" => "Meta and Egg"],
-        ["type" => "Ethiopian Pizza", "price" => "57", "base" => "Meta and Egg"],
-        ["type" => "Ethiopian Pizza", "price" => "57", "base" => "Meta and Egg"]
-    ];
 
-    return view('Pizza', ['pizzas' => $pizzas, 'name' =>request('name'), 'age' => request('age')],);
+
+Route::get('/pizza', [PizzaController::class, 'index']);
+
+
+
+Route::get('/pizza/{id}', function ($id) {
+   
+    return view('details',['id'=> $id]);
 });
-
 
